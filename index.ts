5497ea42e561c360033beb0c6e51b145b3f096d8
Aya Soghayyer <soghayyeraya@gmail.com>
2023-10-09 17:49:41 +0300

    first

---
+++ index.ts
@@ -0,0 +1,21 @@
+import "reflect-metadata"
+import express from 'express';
+import 'reflect-metadata';
+
+import { Index } from "typeorm";
+
+
+const app = express() ; 
+const PORT = 3000 ; 
+
+app.use(express.json());
+
+ 
+
+app.listen(PORT,() =>{
+console.log(`app is running and listening on port ${PORT}`);
+// db.initialize();
+})
+
+
+export default app

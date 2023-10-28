1a3063875e83cff8026958fedfff73efd45b9185
Waleed Jubeh <wsameer70@gmail.com>
2023-10-28 20:31:01 +0200

    Solve conflict

--- db/dataSource.ts
+++ db/dataSource.ts
@@ -5,12 +5,7 @@ import { Expense } from "./entity/expense";
 import { Currency } from "./entity/currency";
 import { Category } from "./entity/category";
 import { Account } from "./entity/account";
-<<<<<<< HEAD
-import config from '../config'
-import dotenv from 'domain'
-=======
 import dotenv from 'dotenv'
->>>>>>> dev
 
 
 dotenv.config()
@@ -18,22 +13,12 @@ dotenv.config()
 
 const AppDataSource = new DataSource({
     type: 'mysql',
-<<<<<<< HEAD
-    host: 'localhost' ,
-    port: 3306,
-    username: 'root',
-    password: '',
-    database: 'expense_tracker',
-    entities: [  Business, Expense, Personal, Currency, Category, Account],
-=======
     host: process.env.DB_HOST ,
     port: Number(process.env.DB_PORT),
     username: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
-   
     entities: [   Expense, Currency, Category,Account],
->>>>>>> dev
     // migrations: ['./**/migration/*.ts'],
     synchronize: true,
     logging: true

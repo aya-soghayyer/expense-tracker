6f7c6cdc04154cfd163f08c018ad40b5357512a9
Tarteel Devops <tarteel.ghassan1@gmail.com>
2023-10-29 18:12:17 +0300

    AWS-configure-S3

---
+++ utils/aws-configur-S3.ts
@@ -0,0 +1,23 @@
+import AWS from 'aws-sdk';
+
+const configureS3Bucket = async () => {
+    const accessKeyId = process.env.AWS_ACCESS_KEY_ID;
+    const secretAccessKey = process.env.AWS_SECRET_ACCESS_KEY;
+
+    if (!accessKeyId || !secretAccessKey) {
+        throw 'AWS access key ID or secret access key is undefined.'
+    }
+
+    AWS.config.update({
+        credentials: {
+            accessKeyId,
+            secretAccessKey,
+        },
+        region: process.env.AWS_REGION,
+    });
+
+    const S3 = new AWS.S3();
+    return S3;
+}
+
+export { configureS3Bucket };

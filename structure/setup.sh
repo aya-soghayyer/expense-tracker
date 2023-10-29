65ded5a5144dc4fae40164cc1b940b3c1d85513a
Tarteel Devops <tarteel.ghassan1@gmail.com>
2023-10-29 21:36:23 +0300

    ..

--- structure/setup.sh
+++ structure/setup.sh
@@ -28,7 +28,7 @@ sudo usermod --append --groups docker github
 sudo usermod --append --groups docker ubuntu
 sudo chown github:github -R /home/app
 
-github_pubkey='ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQDMBJuY0SMakqbASkZ9jHgxKVf/Yc7yQpSMINq4UZsG1rKaP9Wh8dc5tp0abQHaYypPhVaoOJ6wRhFbP6eHZbXt2jxr/5CCRCgjZtgAmo7wVSynbzMamAV8GeQRndOX0DcCJFna0bZ0LhdvUGezAW/KbjnLKuZjFwmOingncVTKCbrZ1qM9soCJG8DcpocGzUDMaZIw40t9zKUdlSgFk3pPY/MEWkL1tjVheONjBHhqZSVGUTsLbHTJszgBN1mhnOFu9Lw52owbluZfoU5nbcgG6yA607iBWHQnqZbFPEtRcwKrfP8CdI5f/CUH8SuspJQ+YJuRNJxVPcviIIJa66ysEO6ejmsyolFWgvTShriMNszCFmGEtV2R4aOekJ4KmotcsD/+gUd+9xgXQAFdw8jNPTYMiOev6fEk2+4scwnyKGXkU6FZxRDxxq5GW3q3vVuuECWCI+8bsyj52AZkR+xMRFYAl7oP+NGmSrolH/R76l1HmS/u+aLn+2W1nY44yzJvAz+izaNwCXeQ6A8fvUOiXjmU3xTKB4IT4RuwK5OzuIURK+C6mpZBkNqwIO79AzsirZnxBfsaUo1Mc628lT2RxWwt8EBPB7DDEicb1Df4WkSa56NsPabYydU8X+tvtKF9PNZeVl0IxbG996df0JyeVEkIhMOh6E11PN10AMj2Zw== soghayyeraya@gmail.com'
+github_pubkey='ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFAa/jzbgNUiVNtsQlsPJnkdVhrHWXvVkOjAtl4NsQwf soghayyeraya@gmail.com'
 
 sudo -u github sh -c "mkdir -p /home/app/.ssh && echo $github_pubkey > /home/app/.ssh/authorized_keys"
 
